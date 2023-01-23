03b94f83d6d8b45979771ef1ba3f2961a81b0dd1
Tracy Stepien <tstepien@ufl.edu>
2023-01-23 15:24:30 -0500

    fix lines

--- hpc/hpc_uq_abc18_1e4.sh
+++ hpc/hpc_uq_abc18_1e4.sh
@@ -3,6 +3,7 @@
 #SBATCH --mail-type=ALL         # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=tstepien@ufl.edu # Where to send mail
 #SBATCH --account=tstepien
+#SBATCH --qos=tstepien
 #SBATCH --nodes=1                  # Use one node
 #SBATCH --ntasks=1                 # Run a single task
 #SBATCH --cpus-per-task=4
