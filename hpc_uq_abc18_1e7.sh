f9daa57ccd6bbb8903dbc003f14bba0cd668469f
Tracy Stepien <tstepien@ufl.edu>
2023-01-24 15:44:09 -0500

    update slurm

--- hpc_uq_abc18_1e7.sh
+++ hpc_uq_abc18_1e7.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1                  # Use one node
 #SBATCH --ntasks=1                 # Run a single task
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=2gb          # Memory per processor
+#SBATCH --mem-per-cpu=4gb          # Memory per processor
 #SBATCH --time=24:00:00             # Time limit hrs:min:sec
 #SBATCH --output=logs/MATLAB_%j.txt    # Output and error log
 pwd; hostname; date
