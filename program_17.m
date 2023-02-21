412e2e6b8e44379a300fcd100fabecb246f1f04a
Tracy Stepien <tstepien@ufl.edu>
2023-02-21 16:18:42 -0500

    add single sims for inversion results

---
+++ program_17.m
@@ -0,0 +1,62 @@
+clear variables global;
+clc;
+close all;
+addpath plot_simulations
+
+%%% time unit: hr
+%%% space unit: mm
+
+%%%%%%%%%%%%%%%%%%%%%%%%%% astrocyte parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%
+mu = 4.8; %%% adhesion constant
+alpha10 = 1.1; %%% (/hr) basal proliferation rate APC
+alpha11 = 1; %%% (/hr) proliferation rate APC wrt PDGFA
+alpha12 = 0.52; %%% (/hr) proliferation rate APC wrt choroid oxygen
+alpha13 = 1.1; %%% (/hr) proliferation rate APC wrt hylaoid oxygen
+alpha20 = 0.98; %%% (/hr) basal proliferation rate IPA
+alpha21 = 1; %%% (/hr) proliferation rate IPA wrt PDGFA
+alpha22 = 0.47; %%% (/hr) proliferation rate IPA wrt choroid oxygen
+alpha23 = 1; %%% (/hr) proliferation rate IPA wrt hyaloid oxygen
+beta0 = 0.85; %%% (/hr) basal differentiation rate
+beta1 = 0.99; %%% (/hr) differentiation rate wrt LIF
+beta2 = 0.97; %%% (/hr) differentiation rate wrt choroid oxygen
+beta3 = 0; %%% (/hr) differentiation rate wrt hyaloid oxygen
+beta4 = 1.1; %%% (/hr) mass action rate
+eta1 = 0.85; %%% (/hr) apoptosis rate APC
+eta2 = 1; %%% (/hr) apoptosis rate IPA
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% hyaloid artery %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+P_hy = 12; %%% partial pressure of oxygen due to hyaloid artery
+r_hy = 0.83; %%% radius at half-maximum of Hill function for hyaloid
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% mesh parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+m.dr = 0.01;
+m.rmax = 5; %%% max radius (mm) (estimate rat retinal radius = 4.1 mm)
+m.tmax = 7*24; %%% max time (hr) (7 days = 168 hr)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% solve equation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+tic
+[t,r,c1,c2,q1,q2,mvgbdy,vel_cir,vel_rad] = eqnsolver(mu,alpha10,alpha11,...
+    alpha12,alpha13,alpha20,alpha21,alpha22,alpha23,beta0,beta1,beta2,...
+    beta3,beta4,eta1,eta2,P_hy,r_hy,m);
+toc
+
+
+%% error
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% error calculation %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[err_tot,err_time,err_rad,err_dens] = errorfunction(t,r,mvgbdy,c1,c2);
+
+
+%% area under curve - trapezoidal rule
+%%%%%%%%%%%%%%%%%%%%%% testing: conservation of mass %%%%%%%%%%%%%%%%%%%%%%
+areaundercurve = zeros(length(t),1);
+dr = m.dr;
+k = c1+c2;
+for i = 1:length(t)
+    areaundercurve(i) = dr*sum( k(1:mvgbdy(1)/dr+(i-1)) ...
+        + k(2:mvgbdy(1)/dr+1+(i-1)) ) / 2;
+end
+
+%% plots
+plot_the_plots
+plot_the_plots_APCIPA
+plot_movingbdy
