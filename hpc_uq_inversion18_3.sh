62ac0b5993ca5c3b0ec4383ca26d25f533269622
Tracy Stepien <tstepien@ufl.edu>
2023-03-09 10:29:09 -0500

    add sh batch files

--- hpc_uq_inversion18_3.sh
+++ hpc_uq_inversion18_3.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=uq_inversion18
+#SBATCH --job-name=uq_inversion18_3
 #SBATCH --mail-type=ALL         # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=tstepien@ufl.edu # Where to send mail
 #SBATCH --account=tstepien
@@ -13,7 +13,7 @@
 pwd; hostname; date
 
 module load matlab
-./uq_inversion18
+./uq_inversion18_3
 
 date
 
