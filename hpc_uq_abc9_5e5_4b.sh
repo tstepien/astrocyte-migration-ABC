5c305cc8b3ff39bb8fa08ca0e6ee9b12b26b0d30
Tracy Stepien <tstepien@ufl.edu>
2023-05-01 11:12:10 -0400

    fix typo

--- hpc_uq_abc9_5e5_4b.sh
+++ hpc_uq_abc9_5e5_4b.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=uq_abc9_5e5_4a
+#SBATCH --job-name=uq_abc9_5e5_4b
 #SBATCH --mail-type=ALL         # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=tstepien@ufl.edu # Where to send mail
 #SBATCH --account=tstepien
@@ -13,7 +13,7 @@
 pwd; hostname; date
 
 module load matlab
-./uq_abc9_5e5_4a
+./uq_abc9_5e5_4b
 
 date
 
