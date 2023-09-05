14ac61f2c84c97efdaf18dd6ab384c52fbfa3dd2
Tracy Stepien <tstepien@c0711a-s30.ufhpc>
2023-09-05 13:43:07 -0400

    update iterations

--- uq_abc18_5e5_5.m
+++ uq_abc18_5e5_5.m
@@ -87,7 +87,7 @@ err_rad = zeros(N,1);
 err_dens = zeros(N,1);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% run simulations %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-parfor i=1+8e5:1e5
+parfor i=1+8e4:1e5
     disp(['iteration i: ',num2str(i)])
     %%% solve equation
     [t,r,c1,c2,~,~,mvgbdy,~,~] = eqnsolver(mu(i),alpha10(i),alpha11(i),...
