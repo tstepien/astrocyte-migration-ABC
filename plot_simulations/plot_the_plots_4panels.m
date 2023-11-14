6b6c43d637e7fdfcd1dc46472bd0adcd19f67aa1
Tracy Stepien <tstepien@ufl.edu>
2023-11-14 10:32:12 -0500

    add simulations with best fit parameters

---
+++ plot_simulations/plot_the_plots_4panels.m
@@ -0,0 +1,152 @@
+parameters_fixed
+
+%%% max distance astrocytes spread
+max_astrocytes = 2.67;
+
+rad_APC = [0.17; 0.33; 0.5; 0.67; 1.67; 2.17; 2.67]; %not including E17
+rad_days = [0; 1; 3; 4; 5; 6; 7];
+
+%%% set up the plots
+%%% variables
+R = length(r);
+dr = r(2)-r(1);
+rmax = m.rmax;
+j_init = s0/dr+1;
+
+%%% cell layer thickness and radius
+[thickness_ret,~,~,~] = thick_rad(t,r);
+
+%%% variables
+T = length(t);
+
+%%% only plot a subset of the times
+% numcurvesplot = 7;
+% if T<=numcurvesplot
+%     plotind = 1:T;
+%     numcurvesplot = length(plotind);
+% else
+%     plotind = floor(linspace(1,T,numcurvesplot));
+% end
+
+%%% plot times: day 0, 1, 2, 3, 4, 5, 6, 7
+numcurvesplot = 8;
+plotind = zeros(1,numcurvesplot);
+for i=0:numcurvesplot-1
+    plotind(i+1) = find(abs((t/24-i))==min(abs(t/24-i)));
+end
+
+%%% color order
+co = [0    0.4470    0.7410
+    0.8500    0.3250    0.0980
+    0.9290    0.6940    0.1250
+    0.4940    0.1840    0.5560
+    0.4660    0.6740    0.1880
+    0.3010    0.7450    0.9330
+    0.6350    0.0780    0.1840
+    0         0.3725    0];
+
+%%% make moving boundary sharp on the plot
+c1plot = zeros(T,R+1);
+c2plot = zeros(T,R+1);
+vel_cirplot = zeros(T,R+1);
+vel_radplot = zeros(T,R+1);
+tension_plot = zeros(T,R+1);
+rplot = zeros(T,R+1);
+for i = 1:T
+    c1plot(i,:) = [c1(i,1:j_init+(i-1)) , zeros(1,R+1-(j_init+(i-1)))];
+    c2plot(i,:) = [c2(i,1:j_init+(i-1)) , zeros(1,R+1-(j_init+(i-1)))];
+    vel_cirplot(i,:) = [vel_cir(i,1:j_init+(i-1)) , zeros(1,R+1-(j_init+(i-1)))];
+    vel_radplot(i,:) = [vel_rad(i,1:j_init+(i-1)) , zeros(1,R+1-(j_init+(i-1)))];
+    tension_plot(i,:) = [kappa*(1./sqrt(pi*(c1plot(i,1:j_init+(i-1))...
+        + c2plot(i,1:j_init+(i-1)))) - rbar) , zeros(1,R+1-(j_init+(i-1)))];
+    
+    rplot(i,:) = [r(1:j_init+(i-1)) , r(j_init+(i-1)) , r(j_init+i:end)];
+end
+
+fslabel = 16;
+fsticks = 14;
+
+%% plot cell concentrations
+
+figure
+tiledlayout(1,4,'TileSpacing','tight','Padding','tight')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% total cell population
+nexttile
+hold on
+for i=1:numcurvesplot
+    plot(rplot(plotind(i),:),c1plot(plotind(i),:)+c2plot(plotind(i),:),...
+        'LineWidth',1.5,'Color',co(i,:))
+end
+% ylim_sum = get(gca,'YLim');
+    ylim_sum = [0,1500];
+line([max_astrocytes,max_astrocytes],ylim_sum,'LineStyle','--',...
+    'Color',[0.5,0.5,0.5],'LineWidth',1.25)
+hold off
+xlabel('radius (mm)','FontSize',fslabel)
+ylabel('APCs + IPAs (cells/mm^2)','FontSize',fslabel)
+if mvgbdy(end)<1.5
+    set(gca,'XLim',[0,mvgbdy(end)+5*dr],'FontSize',fsticks)
+else
+    set(gca,'XLim',[0,rmax],'FontSize',fsticks)
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% APCs
+nexttile
+hold on
+for i=1:numcurvesplot
+    plot(rplot(plotind(i),:),c1plot(plotind(i),:),'LineWidth',1.5,...
+        'Color',co(i,:))
+end
+line([max_astrocytes,max_astrocytes],ylim_sum,'LineStyle','--',...
+    'Color',[0.5,0.5,0.5],'LineWidth',1.25)
+hold off
+xlabel('radius (mm)','FontSize',fslabel)
+ylabel('APCs (cells/mm^2)','FontSize',fslabel)
+if mvgbdy(end)<1.5
+    set(gca,'XLim',[0,mvgbdy(end)+5*dr],'FontSize',fsticks,'YLim',ylim_sum)
+else
+    set(gca,'XLim',[0,rmax],'FontSize',fsticks,'YLim',ylim_sum)
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% IPAs
+nexttile
+hold on
+for i=1:numcurvesplot
+    plot(rplot(plotind(i),:),c2plot(plotind(i),:),'LineWidth',1.5,...
+        'Color',co(i,:))
+end
+line([max_astrocytes,max_astrocytes],ylim_sum,'LineStyle','--',...
+    'Color',[0.5,0.5,0.5],'LineWidth',1.25)
+hold off
+xlabel('radius (mm)','FontSize',fslabel)
+ylabel('IPAs (cells/mm^2)','FontSize',fslabel)
+if mvgbdy(end)<1.5
+    set(gca,'XLim',[0,mvgbdy(end)+5*dr],'FontSize',fsticks,'YLim',ylim_sum)
+else
+    set(gca,'XLim',[0,rmax],'FontSize',fsticks,'YLim',ylim_sum)
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% moving boundary
+nexttile
+hold on
+plot(t/24,mvgbdy,'k','LineWidth',1.5)
+scatter(t/24,mvgbdy,20,'k')
+scatter(rad_days,rad_APC,150,[0.5 0.5 0.5],'x','LineWidth',1.5)
+hold off
+xlabel('t (days)','FontSize',fslabel)
+ylabel('moving boundary (mm)','FontSize',fslabel)
+ylim_mvgbdy = get(gca,'YLim');
+if ylim_mvgbdy(2)<max_astrocytes
+    set(gca,'FontSize',fsticks,'YLim',[0,max_astrocytes])
+else
+    set(gca,'FontSize',fsticks,'XLim',[0,7])
+    xticks([0 1 2 3 4 5 6 7])
+end
+
+
+set(gcf,'Units','inches','Position',[2,2,14,3.25],'PaperPositionMode','auto')
+
---
+++ program_bestfitparam.m
@@ -0,0 +1,49 @@
+clear variables global;
+clc;
+close all;
+addpath plot_simulations
+
+%%% time unit: hr
+%%% space unit: mm
+
+oxyfunc = 'oxygen_zeroorder';
+
+%%%%%%%%%%%%%%%%%%%%%% loading a parameter set file %%%%%%%%%%%%%%%%%%%%%%%
+load('parameter_analysis/bestfitparam18.mat')
+mu = param_new(1); %%% adhesion constant
+alpha10 = param_new(2); %%% (/hr) basal proliferation rate APC
+alpha11 = param_new(3); %%% (/hr) proliferation rate APC wrt PDGFA
+alpha12 = param_new(4); %%% (/hr) proliferation rate APC wrt choroid oxygen
+alpha13 = param_new(5); %%% (/hr) proliferation rate APC wrt hylaoid oxygen
+alpha20 = param_new(6); %%% (/hr) basal proliferation rate IPA
+alpha21 = param_new(7); %%% (/hr) proliferation rate IPA wrt PDGFA
+alpha22 = param_new(8); %%% (/hr) proliferation rate IPA wrt choroid oxygen
+alpha23 = param_new(9); %%% (/hr) proliferation rate IPA wrt hyaloid oxygen
+beta0 = param_new(10); %%% (/hr) basal differentiation rate
+beta1 = param_new(11); %%% (/hr) differentiation rate wrt LIF
+beta2 = param_new(12); %%% (/hr) differentiation rate wrt choroid oxygen
+beta3 = param_new(13); %%% (/hr) differentiation rate wrt hyaloid oxygen
+beta4 = param_new(14); %%% (/hr) mass action rate
+eta1 = param_new(15); %%% (/hr) apoptosis rate APC
+eta2 = param_new(16); %%% (/hr) apoptosis rate IPA
+P_hy = param_new(17); %%% partial pressure of oxygen due to hyaloid artery
+r_hy = param_new(18); %%% radius at half-maximum of Hill function for hyaloid
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% mesh parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+m.dr = 0.01;
+m.rmax = 5; %%% max radius (mm) (estimate rat retinal radius = 4.1 mm)
+m.tmax = 7*24; %%% max time (hr) (7 days = 168 hr)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% solve equation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+tic
+[t,r,c1,c2,q1,q2,mvgbdy,vel_cir,vel_rad,choroidPO2] = eqnsolver(mu,...
+    alpha10,alpha11,alpha12,alpha13,alpha20,alpha21,alpha22,alpha23,...
+    beta0,beta1,beta2,beta3,beta4,eta1,eta2,P_hy,r_hy,m,oxyfunc);
+toc
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% error calculation %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[err_tot,err_time,err_rad,err_dens,err_flag] = errorfunction(t,r,mvgbdy,c1,c2);
+disp(['total error: ',num2str(err_tot)])
+
+%% plots
+plot_the_plots_4panels
