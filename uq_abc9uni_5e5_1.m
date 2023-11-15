202a3ee7945d9043ff88149ad8664b9767a970ef
Tracy Stepien <tstepien@ufl.edu>
2023-11-15 12:52:54 -0500

    add 9 parameter analysis

--- uq_abc9uni_5e5_1.m
+++ uq_abc9uni_5e5_1.m
@@ -6,9 +6,9 @@ clc;
 multiplier = 5;
 power = 5;
 N = (multiplier)*10^(power);
-num_param = 10;
+num_param = 9;
 
-iternum = '4';
+iternum = '1';
 
 oxyfunc = 'oxygen_zeroorder';
 
@@ -24,13 +24,13 @@ oxyfunc = 'oxygen_zeroorder';
 
 
 %%%%%%%%%%%%%%%%%%%%%%% load Latin Hypercube points %%%%%%%%%%%%%%%%%%%%%%%
-load(strcat(pwd,'/LHpts/LHpts',num2str(num_param),'_',num2str(multiplier),...
+load(strcat(pwd,'/LHpts/LHpts',num2str(num_param),'uni_',num2str(multiplier),...
     'e',num2str(power),'.mat'))
 
 % if strcmp(savefiles,'yes')==1
 %     doublecheck = input('Are you sure you would like to save the output files? (it may overwrite): ');
 %     if strcmp(doublecheck,'y')==1
-        diary(strcat(pwd,'/parameter_analysis/diary_abc',num2str(num_param),'_',...
+        diary(strcat(pwd,'/parameter_analysis/diary_abc',num2str(num_param),'uni_',...
             num2str(multiplier),'e',num2str(power),'_',iternum,'.txt'));
 %     else
 %         return;
@@ -43,42 +43,42 @@ m.rmax = 5; %%% max radius (mm) (estimate rat retinal radius = 4.1 mm)
 m.tmax = 7*24; %%% max time (hr) (7 days = 168 hr)
 
 %%%%%%%%%%%%%%%%%%%%%%%% parameters to investigate %%%%%%%%%%%%%%%%%%%%%%%%
-bound = [0.1 100; %mu - adhesion constant
-    0 2; %alpha10 - (/hr) base proliferation rate APC
-    0 2; %alpha11 - (/hr) proliferation rate APC wrt PDGFA
-    0 2; %alpha12 - (/hr) proliferation rate APC wrt choroid oxygen
-    % 0 2; %alpha13 - (/hr) proliferation rate APC wrt hyaloid oxygen
-    0 2; %alpha20 - (/hr) base proliferation rate IPA
-    0 2; %alpha21 - (/hr) proliferation rate IPA wrt PDGFA
-    % 0 2; %alpha22 - (/hr) proliferation rate IPA wrt choroid oxygen
-    % 0 2; %alpha23 - (/hr) proliferation rate IPA wrt hyaloid oxygen
-    0 3; %beta0 - (/hr) base differentiation rate
-    0 3; %beta1 - (/hr) differentiation rate wrt LIF
-    % 0 3; %beta2 -  (/hr) differentiation rate wrt choroid oxygen
-    % 0 3; %beta3 -  (/hr) differentiation rate wrt hyaloid oxygen
-    0 3; %beta4 -  (/hr) mass action rate
-    % 0 2; %eta1 - (/hr) apoptosis rate APC
-    0 2]; %eta2 - (/hr) apoptosis rate IPA
+bound = [5 20; %mu - adhesion constant
+    0 0.1; %alpha10 - (/hr) base proliferation rate APC
+    % 0 0.1; %alpha11 - (/hr) proliferation rate APC wrt PDGFA
+    0 0.1; %alpha12 - (/hr) proliferation rate APC wrt choroid oxygen
+    % 0 0.1; %alpha13 - (/hr) proliferation rate APC wrt hyaloid oxygen
+    0 0.1; %alpha20 - (/hr) base proliferation rate IPA
+    0 0.1; %alpha21 - (/hr) proliferation rate IPA wrt PDGFA
+    0 0.1; %alpha22 - (/hr) proliferation rate IPA wrt choroid oxygen
+    % 0 0.1; %alpha23 - (/hr) proliferation rate IPA wrt hyaloid oxygen
+    % 0 0.1; %beta0 - (/hr) base differentiation rate
+    0 0.1; %beta1 - (/hr) differentiation rate wrt LIF
+    0 0.1; %beta2 -  (/hr) differentiation rate wrt choroid oxygen
+    % 0 0.1; %beta3 -  (/hr) differentiation rate wrt hyaloid oxygen
+    % 0 0.1; %beta4 -  (/hr) mass action rate
+    % 0 0.1; %eta1 - (/hr) apoptosis rate APC
+    0 0.1]; %eta2 - (/hr) apoptosis rate IPA
     % 0 20; %P_hy - partial pressure of oxygen due to hyaloid artery
-    % 0.001 2]; %r_hy - radius at half-maximum of Hill function for hyaloid
+    % 0.1 2]; %r_hy - radius at half-maximum of Hill function for hyaloid
 numpar = length(bound);
 
 mu      = (bound(1,2) - bound(1,1))*LHpts(:,1) + bound(1,1);
 alpha10 = (bound(2,2) - bound(2,1))*LHpts(:,2) + bound(2,1);
-alpha11 = (bound(3,2) - bound(3,1))*LHpts(:,3) + bound(3,1);
-alpha12 = (bound(4,2) - bound(4,1))*LHpts(:,4) + bound(4,1);
+alpha11 = 0;
+alpha12 = (bound(3,2) - bound(3,1))*LHpts(:,3) + bound(3,1);
 alpha13 = 0;
-alpha20 = (bound(5,2) - bound(5,1))*LHpts(:,5) + bound(5,1);
-alpha21 = (bound(6,2) - bound(6,1))*LHpts(:,6) + bound(6,1);
-alpha22 = 0;
+alpha20 = (bound(4,2) - bound(4,1))*LHpts(:,4) + bound(4,1);
+alpha21 = (bound(5,2) - bound(5,1))*LHpts(:,5) + bound(5,1);
+alpha22 = (bound(6,2) - bound(6,1))*LHpts(:,6) + bound(6,1);
 alpha23 = 0;
-beta0   = (bound(7,2) - bound(7,1))*LHpts(:,7) + bound(7,1);
-beta1   = (bound(8,2) - bound(8,1))*LHpts(:,8) + bound(8,1);
-beta2   = 0;
-beta4   = (bound(9,2) - bound(9,1))*LHpts(:,9) + bound(9,1);
+beta0   = 0;
+beta1   = (bound(7,2) - bound(7,1))*LHpts(:,7) + bound(7,1);
+beta2   = (bound(8,2) - bound(8,1))*LHpts(:,8) + bound(8,1);
+beta4   = 0;
 beta3   = 0;
 eta1    = 0;
-eta2    = (bound(10,2) - bound(10,1))*LHpts(:,10) + bound(10,1);
+eta2    = (bound(9,2) - bound(9,1))*LHpts(:,9) + bound(9,1);
 P_hy    = 0;
 r_hy    = 1;
 
@@ -90,12 +90,12 @@ err_dens = zeros(N,1);
 err_flag = cell(N,1);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% run simulations %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-parfor i=1+3e5:4e5
+parfor i=1:1e5
     disp(['iteration i: ',num2str(i)])
     %%% solve equation
-    [t,r,c1,c2,~,~,mvgbdy,~,~] = eqnsolver(mu(i),alpha10(i),alpha11(i),...
-        alpha12(i),alpha13,alpha20(i),alpha21(i),alpha22,alpha23,...
-        beta0(i),beta1(i),beta2,beta3,beta4(i),eta1,eta2(i),...
+    [t,r,c1,c2,~,~,mvgbdy,~,~] = eqnsolver(mu(i),alpha10(i),alpha11,...
+        alpha12(i),alpha13,alpha20(i),alpha21(i),alpha22(i),alpha23,...
+        beta0,beta1(i),beta2(i),beta3,beta4,eta1,eta2(i),...
         P_hy,r_hy,m,oxyfunc);
     
     %%% error calculation
@@ -104,7 +104,7 @@ parfor i=1+3e5:4e5
 end
 
 % if strcmp(savefiles,'yes')==1
-    save(strcat(pwd,'/parameter_analysis/abc',num2str(num_param),'_',...
+    save(strcat(pwd,'/parameter_analysis/abc',num2str(num_param),'uni_',...
         num2str(multiplier),'e',num2str(power),'_',iternum,'.mat'));
     diary off
 % end
